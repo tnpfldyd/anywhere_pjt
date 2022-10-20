bd208958d30ad876b195e8e770a2dbfda66fd6ec
tnpfldyd <dkdldhs0322@gmail.com>
2022-10-20 15:18:11 +0900

    yh

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,9 @@ app_name = "accounts"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.signin, name="login"),
+    path("logout/", views.signout, name="logout"),
+    path("<str:username>/", views.profile, name="profile"),
+    path("profile_edit/", views.profile_edit, name="profile_edit"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from .forms import CustomUserCreationForm, ProfileForm, CustomUserChangeForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .models import Profile
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "accounts/index.html")
+
+
+def profile(request, username):
+    info = get_object_or_404(get_user_model(), username=username)
+    return render(request, "accounts/profile.html", {"person": info})
+
+
+def signup(request):
+    form = CustomUserCreationForm(request.POST or None)
+    if form.is_valid():
+        login(request, form.save())
+        return redirect("accounts:index")
+    return render(request, "accounts/signup.html", {"form": form})
+
+
+def signin(request):
+    form = AuthenticationForm(request, data=request.POST or None)
+    if form.is_valid():
+        login(request, form.get_user())
+        return redirect("accounts:index")
+    return render(request, "accounts/login.html", {"form": form})
+
+
+def signout(request):
+    logout(request)
+    return redirect("articles:index")
+
+
+def profile_edit(request):
+    if request.method == "POST":
+        user_change_form = CustomUserChangeForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
