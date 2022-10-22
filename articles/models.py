fdd10fdbd33f3f2dd284e93ea21f5cb0015b09f5
tnpfldyd <dkdldhs0322@gmail.com>
2022-10-22 23:26:19 +0900

    chat edit

--- articles/models.py
+++ articles/models.py
@@ -8,19 +8,22 @@ class Article(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
     content = models.TextField()
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to="images/", blank=True)
     thumbnail = ProcessedImageField(
-        upload_to='thumbnail/',
+        upload_to="thumbnail/",
         blank=True,
         processors=[Thumbnail(300, 200)],
-        format='JPEG',
-        options={'quality':90},
+        format="JPEG",
+        options={"quality": 90},
     )
+    article_liking = models.ManyToManyField("self", symmetrical=False, related_name="article_liker")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
+
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comment_liking = models.ManyToManyField("self", symmetrical=False, related_name="comment_liker")
     content = models.CharField(max_length=80)
     created_at = models.DateTimeField(auto_now_add=True)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
